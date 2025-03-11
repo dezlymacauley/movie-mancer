d33c1c5ab25d74d7b5b24c868991b2addca68300
dezlymacauley <dezlymacauley@proton.me>
2025-03-11 14:12:09 +0200

    Learned how to setup NativeWind and custom colours

---
+++ 00_tailwindcss_reference.md
@@ -0,0 +1,11 @@
+```tsx
+<View
+    className="flex-1"
+>
+
+</View>
+```
+
+flex-1 makes <View> fill the entire screen or parent.
+
+It's shorthand for style={{ flex: 1 }}
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,19 @@
+// This imports components from the  React Native library 
 import { Text, View } from "react-native";
+
+
+import "./globals.css";
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      className="flex-1 justify-center items-center"
     >
-      <Text>Movie Mancer</Text>
+      <Text
+        className="text-5xl text-dark-200 font-bold"
+      >
+        Movie Mancer 
+      </Text>
     </View>
   );
 }
